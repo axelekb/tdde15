14a27327a6998b7b71e3a744bef3b9454e389ddd
Axel Ekblomgit config --global user.name axeek668git config --global user.name Axel <axeek668@student.liu.se>
2022-09-05 18:21:35 +0200

    exc 3

--- lab1.R
+++ lab1.R
@@ -17,11 +17,37 @@ train = asia[id,]
 test = asia[-id,]
 
 library(gRain)
-bn = bn.fit(hc(test), test)
+bn = bn.fit(hc(train), train)
+true_bn = bn.fit(model2network("[A][S][T|A][L|S][B|S][D|B:E][E|T:L][X|E]"), train)
 
+cond_probs = matrix(ncol=2, nrow=dim(test)[1])
+colnames(cond_probs)=c("learnt", "true")
+for (i in 1:dim(test)[1]) {
+  cond_probs[i,1] = cpquery(bn, (S=="yes"), ((A==test[i,1]) & (T==test[i,3]) & (L==test[i,4]) & (B==test[i,5]) & (E==test[i,6]) & (X==test[i,7]) & (D==test[i,8])))
+  cond_probs[i,2] = cpquery(true_bn, (S=="yes"), ((A==test[i,1]) & (T==test[i,3]) & (L==test[i,4]) & (B==test[i,5]) & (E==test[i,6]) & (X==test[i,7]) & (D==test[i,8])))
+}
+
+pred_learnt = ifelse(cond_probs[,1]>0.5, "yes", "no")
+cm_learnt = table(pred_learnt, test[,"S"])
+cm_learnt
+sum(diag(cm_learnt))/sum(cm_learnt)
+
+pred_true = ifelse(cond_probs[,2]>0.5, "yes", "no")
+cm_true = table(pred_true, test[,"S"])
+cm_true
+sum(diag(cm_true))/sum(cm_true)
+
+# 3)
+graphviz.plot(bn)
+mb(bn, "E")
 cond_probs = c()
 for (i in 1:dim(test)[1]) {
-  cond_probs[i] = cpquery(bn, (S=="yes"), ((A==test[i,1]) & (T==test[i,3]) & (L==test[i,4]) & (B==test[i,5]) & (E==test[i,6]) & (X==test[i,7]) & (D==test[i,8])))
+  cond_probs[i] = cpquery(bn, (S=="yes"), ((L==test[i,4]) & (B==test[i,5])))
+  
 }
 pred = ifelse(cond_probs>0.5, "yes", "no")
-table(pred, test[,"S"])
+cm = table(pred_true, test[,"S"])
+cm
+sum(diag(cm))/sum(cm)
+
+# 4)
